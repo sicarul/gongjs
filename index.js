0d529fb6cea314c5a778d7802b87c744e383881e
Pablo Seibelt <pablo.seibelt@auth0.com>
2018-08-27 21:37:36 +0000

    Use cache

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const spawnSync = require('child_process').spawnSync;
 const Pusher = require('pusher-js');
+const NodeCache = require('node-cache');
+const myCache = new NodeCache();
 
 // pigs s 2 1000
 // sleep 0.2
@@ -23,15 +25,25 @@ const startStep = 800;
 pigs('2', startStep);
 
 channel.bind('hit', function(data) {
-  console.log(`gong:hit: ${JSON.stringify(data)}`);
-  const count = Math.min(3, data.count);
-  const delay = data.delay || 0.2; //Math.max(0.15, data.delay || 0);
+  myCache.get('lastGong', function(err, value){
+    var threshold = 900000;
+    if(count==17){
+      var threshold=1800000 // 30 min
+    }
 
-  for(var i = 0; i < count; i++) {
-    pigs('2', '1100');
-    sleep('0.15');
-    pigs('2', startStep);
-    sleep(delay);
+    if(err || Date.now() - value > threshold){ // At least 15 min
+      myCache.set( "lastGong", Date.now(), function( err, success ){
+        console.log(`gong:hit: ${JSON.stringify(data)}`);
+        const count = Math.min(3, data.count);
+        const delay = data.delay || 0.2; //Math.max(0.15, data.delay || 0);
+
+        for(var i = 0; i < count; i++) {
+          pigs('2', '1100');
+          sleep('0.15');
+          pigs('2', startStep);
+          sleep(delay);
+        }
+      }
+    }
   }
 });
-
